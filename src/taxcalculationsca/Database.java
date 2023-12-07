2c82f0ed3e901ccec8e71e95a2f39872a428b5eb
2022236 <2022236@student.cct.ie>
2023-12-07 00:14:44 +0000

    Database abstract class
    Would be easier to deal with a abstract class once can inherit and help in the process of linking the code with the actual database

--- src/taxcalculationsca/Database.java
+++ src/taxcalculationsca/Database.java
@@ -13,5 +13,8 @@ public abstract class Database {
    protected final static String USER = "OOC2023";
    protected final static String PASSWORD = "ooc2023";
    protected final static String dbName = "TaxCalculation";
-   protected final static String tableName = "user";
+   protected final static String tableName = "userDetails";
+   protected final static String db_URL = dbBaseURL + "/" + dbName;
+   
+   
 }
