c3b846aff9e6e67c92b35793e022a6a9942add40
2022236 <2022236@student.cct.ie>
2023-12-07 00:10:10 +0000

    DatabaseReader
    Is going to read a User in the database

--- src/taxcalculationsca/DatabaseReader.java
+++ src/taxcalculationsca/DatabaseReader.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package taxcalculationsca;
 
-/**
- *
- * @author lizam
- */
-public class DatabaseReader {
-    
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseReader extends Database {
+    public User getUser(String userName, String password) {
+        try (
+            Connection conn = DriverManager.getConnection(db_URL, USER, PASSWORD);
+            Statement stmt = conn.createStatement();  
+        ) {
+            String sql = String.format("SELECT * FROM %s WHERE userName='%s' AND password='%s';", tableName, userName, password);
+            ResultSet results = stmt.executeQuery(sql);
+
+            if (results.next()) {
+                String userRole = results.getString("role");
+                return new User(userName, password, userRole);
+            }
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }              
+    }
+
+   
 }
