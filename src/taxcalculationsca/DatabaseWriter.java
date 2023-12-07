71a037a60550894497b0fe1438f6b0ff6c7965b0
2022236 <2022236@student.cct.ie>
2023-12-07 00:06:43 +0000

    DatabaseWriter
    Responsible for the connection writing with the values assigned

--- src/taxcalculationsca/DatabaseWriter.java
+++ src/taxcalculationsca/DatabaseWriter.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package taxcalculationsca;
 
-/**
- *
- * @author lizam
- */
-public class DatabaseWriter {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import ioutils.IOUtils;
+
+public class DatabaseWriter extends Database {
+    public boolean addUserDatabase(String userName, String role, String email, String password) throws SQLException {
+        IOUtils scan = new IOUtils ();
+        try (
+            Connection conn = DriverManager.getConnection(db_URL, USER, PASSWORD);
+            Statement stmt = conn.createStatement();  
+        ) {
+            String sql = String.format("INSERT INTO " + tableName + " (userName, role, email, password) VALUES ('%s', '%s', '%s', '%s');",
+     userName, role, email, password);
+
+            stmt.execute(sql);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }    
+    }
     
 }
+    
