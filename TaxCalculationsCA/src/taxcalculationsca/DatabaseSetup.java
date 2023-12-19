1bc39e6b2998f93f5502ee9030ca95916d1aa342
2022236 <146198723+2022236@users.noreply.github.com>
2023-12-19 13:30:09 +0000

    Updating database attributes
    Updating the database setup with the right attributes

--- TaxCalculationsCA/src/taxcalculationsca/DatabaseSetup.java
+++ TaxCalculationsCA/src/taxcalculationsca/DatabaseSetup.java
@@ -2,44 +2,48 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-/*package taxcalculationsca;
+package taxcalculationsca;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-*/
+
 
 /**
  *
  * @author lizam
  */
-/*public class DatabaseSetup extends Database {
-    final static String dbBaseURL = "jdbc:mysql://localhost";
-    final static String USER = "OOC2023";
-    final static String PASSWORD = "ooc2023";
-    
+public class DatabaseSetup extends Database {    
     public static boolean setupDB()throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException{
     Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-    try(
-         Connection conn = DriverManager.getConnection(dbBaseURL,USER,PASSWORD);
-         Statement stmt = conn.createStatement();
-            
-            ){
+        try (
+            Connection conn = DriverManager.getConnection(dbBaseURL, USER, PASSWORD);
+            Statement stmt = conn.createStatement();
+        ) {
+            // Create database if it doesn't exist
             stmt.execute("CREATE DATABASE IF NOT EXISTS " + dbName + ";");
             stmt.execute("USE " + dbName + ";");
-            String sql = 
-                "CREATE TABLE IF NOT EXISTS " + tableName + " ("
-                + "userName VARCHAR(255),"
-                + "role VARCHAR(20),"
-                + "email VARCHAR(100),"
-                + "password VARCHAR(12)"
+
+            // SQL to create a table
+            String sql = "CREATE TABLE IF NOT EXISTS " + tableName + " ("
+                + "UserName VARCHAR(255),"
+                + "FirstName VARCHAR(255),"
+                + "LastName VARCHAR(255),"
+                + "Position VARCHAR(255),"
+                + "BirthDate DATE,"
+                + "Email VARCHAR(255),"
+                + "PassWord VARCHAR(255),"
+                + "Type ENUM('Admin', 'Regular'),"
+                + "MaritalStatus ENUM('Single', 'Married', 'Widowed'),"
+                + "PRIMARY KEY (UserName)"
                 + ");";
-        stmt.execute(sql);
-        return true;
-      } catch (Exception e){
-          e.printStackTrace();
-          return false;
+
+            stmt.execute(sql);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
       }   
    }
-}*/
+}
